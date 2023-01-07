8f50bedcd98b9ead134c04b8bab951ec8d083748
GeralmyUrena <geralmyuri11@gmail.com>
2023-01-06 20:34:16 -0500

    GLicth fixed

--- src.js
+++ src.js
@@ -13,7 +13,7 @@ function getData(userName){
         console.log(objec.country[0].country_id )
         let highestProbalityCountry = objec.country[0].country_id
          secondHighestProbabilityCountry = objec.country[1].country_id
-        renderUserInput(secondHighestProbabilityCountry)
+       // renderUserInput(secondHighestProbabilityCountry)
          thridHighestProbabilityCountry = objec.country[2].country_id
         console.log(secondHighestProbabilityCountry)
         console.log(thridHighestProbabilityCountry)
@@ -29,6 +29,7 @@ function getData(userName){
 function submitUserinput(){
     let inputs = document.querySelector("#inputs")
         inputs.addEventListener("submit", function(event) {
+            event.stopPropagation()
         let text = document.querySelector("#search")
         let sub = document.querySelector("#sub")
          event.preventDefault()
@@ -54,56 +55,68 @@ function renderUserInput (country){
     parag.innerText = `You are probably from :` + country
     // let displayedText = parag.innerText
     let addChoices = false
-parag.addEventListener("click", function(){
+
+
+   
+    let field = document.createElement("fieldset")
+    let leg = document.createElement("legend")
+    let division = document.createElement("div")
+   
+    let lab = document.createElement("label")
+    let lab1 = document.createElement("label")
+
+    int = document.createElement("input")
+    int1 = document.createElement("input")
+    int2 =document.createElement("input")       
+
+    let btn4 = document.createElement("button")
+    let lab2 = document.createElement("label")
+parag.addEventListener("click", function(x){
+    x.stopPropagation()
     console.log("are you working?")
-  let popUpFormula = document.querySelector("#formula")
-  let field = document.createElement("fieldset")
-  popUpFormula.append(field)
-  let leg = document.createElement("legend")
-  leg.innerText ="IS YOUR COUNTRY HERE?"
-  field.append(leg)
-  let division = document.createElement("div")
-  field.append(division)
-   int = document.createElement("input")
   
+let popUpFormula = document.querySelector("#formula")
+  
+
+
+  leg.innerText ="IS YOUR COUNTRY HERE?"
+ 
+   
   int.setAttribute("type", "radio" )
   int.setAttribute("name", "countries" )
   int.setAttribute("value", `${secondHighestProbabilityCountry}`)
   int.setAttribute("id", "interpolated")
   
-  let lab = document.createElement("label")
+ 
   lab.setAttribute("for", "interpolated")
   lab.innerText = secondHighestProbabilityCountry
   
-  division.append(int)
-  division.append(lab)
+  
   
 
-   int1 = document.createElement("input")
+  
    int1.setAttribute("type", "radio" )
    int1.setAttribute("name", "countries" )
    int1.setAttribute("value", `${thridHighestProbabilityCountry}`)
    int1.setAttribute("id", "interpolated")
 
-   let lab1 = document.createElement("label")
+   
    lab1.setAttribute("for", "interpolated")
    lab1.innerText =  thridHighestProbabilityCountry
-   division.append(int1)
-   division.append(lab1)
+   
 
-    int2 =document.createElement("input")
+    
    
    int2.setAttribute("type", "radio" )   
     int2.setAttribute("name", "countries" )
    int2.setAttribute("value", "MARS")
    int2.setAttribute("id", "nointerpolated")
 
-   let lab2 = document.createElement("label")
+  
    lab2.setAttribute("for", "nointerpolated")
     lab2.innerText = "no"
 
-    division.append(int2)
-    division.append(lab2)
+   
 
    
 //    let whitin = docment.createElement("div")
@@ -111,13 +124,21 @@ parag.addEventListener("click", function(){
 
 //    field.append(whitin)
 
-   let btn4 = document.createElement("button")
+  
    btn4.setAttribute("type", "submit")
     btn4.innerText = "click"
    
-   popUpFormula.append(btn4)
-   
-
+    field.append(leg)
+    leg.append(division)
+    popUpFormula.append(field) 
+    division.append(int)
+    division.append(lab)
+    division.append(int1)
+    division.append(lab1)
+    division.append(int2)
+    division.append(lab2)
+    
+   popUpFormula.append(btn4) //issue
    renderAnotherInput()
   
 
@@ -144,28 +165,48 @@ parag.addEventListener("click", function(){
   
  }
 )
+
+
+
+
+
+
+  
+ 
+  
+ 
+
+ 
+
+
 }
 
 
 function renderAnotherInput (butt ){
     let form2 = document.querySelector("#formula")
+    let paragrh = document.createElement("p")
 
     form2.addEventListener("submit", function(e){
-
+        e.stopPropagation()
+        
         e.preventDefault()
+      
         //int.setAttribute("value", `${secondHighestProbabilityCountry}`)
         let selected = document.querySelector('input[type ="radio"]:checked');
         // selected.setAttribute("value",`${secondHighestProbabilityCountry}`)
-        let paragrh = document.createElement("p")
-        paragrh.innerText = `WOW! YOU ARE ACTUALLY FROM ${selected.value}`
-        nDev = document.querySelector("#newDiv")
+        
+        paragrh.innerText = `WOW! YOU ARE ACTUALLY FROM ${selected.value}` 
 
-
-        nDev.append(paragrh)
+        
+        
+        
 
     })
 
+    let nDev = document.querySelector("#newDiv")
 
+
+    nDev.append(paragrh)
 //renderAnotherInput()
 
 
